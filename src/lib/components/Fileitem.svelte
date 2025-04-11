7130d2a87a6ece399ebd27025615603e13c9ca14
TheMoonKnight8 <48894025+sajadb-dev@users.noreply.github.com>
2025-04-11 19:59:51 +0330

    removed Svelte-file-dropzone, added meltui file upload, added ui to show uploaded files and select formats

---
+++ src/lib/components/Fileitem.svelte
@@ -0,0 +1,141 @@
+<script lang="ts">
+    import MovieIcon from "~icons/tabler/movie";
+    import AudioIcon from "~icons/tabler/volume";
+    import ImageIcon from "~icons/tabler/photo";
+    import * as Select from "$lib/components/ui/select/index.js";
+    import * as Tabs from "$lib/components/ui/tabs/index.js";
+
+    let { filetype, title} = $props();
+
+    const extensions = {
+      image: [
+        "jpg",
+        "jpeg",
+        "png",
+        "gif",
+        "bmp",
+        "webp",
+        "ico",
+        "tif",
+        "tiff",
+        "svg",
+        "raw",
+        "tga",
+      ],
+      video: [
+        "mp4",
+        "m4v",
+        "mp4v",
+        "3gp",
+        "3g2",
+        "avi",
+        "mov",
+        "wmv",
+        "mkv",
+        "flv",
+        "ogv",
+        "webm",
+        "h264",
+        "264",
+        "hevc",
+        "265",
+      ],
+      audio: ["mp3", "wav", "ogg", "aac", "wma", "flac", "m4a"],
+    };
+    let value = $state("");
+    const defaultValues = "video";
+    let triggerContent = $derived.by(() => {
+        if(value !== "") {
+            if(filetype.includes('image'))
+                return extensions.image.find((f) => f === value)
+            else if(filetype.includes('video'))
+                return extensions.video.find((f) => f === value)
+            else if(filetype.includes('audio'))
+                return extensions.audio.find((f) => f === value)
+        }
+        else
+        return "فرمت"
+
+    });
+</script>
+
+<div class="w-11/12 lg:w-full h-20 px-2 lg:px-12 flex gap-2 lg:gap-6 items-center justify-between rounded-lg border bg-zinc-50 border-slate-300 drop-shadow-md">
+    <div class="flex gap-2 lg:gap-6 items-center">
+        {#if filetype.includes('video')}
+            <MovieIcon class="text-4xl text-amber-500" />
+        {:else if filetype.includes('audio')}
+            <AudioIcon class="text-4xl text-blue-500" />
+        {:else if filetype.includes('image')}
+            <ImageIcon class="text-4xl text-purple-500" />
+        {/if}
+        <div class="flex gap-1">
+            <span>نام :</span>
+            <span class="max-w-[10ch] font-sans truncate">{title}</span>
+        </div>
+    </div>
+    <div class="font-mono">
+    <Select.Root type="single" bind:value>
+        <Select.Trigger class="w-32 outline-none focus:outline-none focus:ring-0 text-center text-muted-foreground bg-background text-md font-medium">
+            {triggerContent}
+        </Select.Trigger>
+        <Select.Content class="h-fit">
+            {#if filetype.includes("image")}
+                <div class="grid grid-cols-2 gap-2 w-fit">
+                    {#each extensions.image as elt}
+                        <div class="col-span-1 text-center">
+                            <Select.Item value={elt} label={elt} class="mx-auto font-mono">
+                                {elt}
+                            </Select.Item>
+                        </div>
+                    {/each}
+                </div>
+            {/if}
+            {#if filetype.includes("video")}
+                <Tabs.Root value={defaultValues} class="w-full">
+                    <Tabs.List class="w-full">
+                        <Tabs.Trigger value="video" class="w-full">
+                            Video
+                        </Tabs.Trigger>
+                        <Tabs.Trigger value="audio" class="w-full">
+                            Audio
+                        </Tabs.Trigger>
+                    </Tabs.List>
+                    <Tabs.Content value="video">
+                        <div class="grid grid-cols-3 gap-2 w-fit">
+                            {#each extensions.video as elt}
+                                <div class="col-span-1 text-center">
+                                    <Select.Item value={elt} label={elt} class="mx-auto font-mono">
+                                        {elt}
+                                    </Select.Item>
+                                </div>
+                            {/each}
+                        </div>
+                    </Tabs.Content>
+                    <Tabs.Content value="audio">
+                        <div class="grid grid-cols-3 gap-2 w-fit">
+                            {#each extensions.audio as elt}
+                                <div class="col-span-1 text-center">
+                                    <Select.Item value={elt} label={elt} class="mx-auto font-mono">
+                                        {elt}
+                                    </Select.Item>
+                                </div>
+                            {/each}
+                        </div>
+                    </Tabs.Content>
+                </Tabs.Root>
+            {/if}
+            {#if filetype.includes("audio")}
+                <div class="grid grid-cols-2 gap-2 w-fit">
+                    {#each extensions.audio as elt}
+                        <div class="col-span-1 text-center">
+                            <Select.Item value={elt} label={elt} class="mx-auto font-mono">
+                                {elt}
+                            </Select.Item>
+                        </div>
+                    {/each}
+                </div>
+            {/if}
+        </Select.Content>
+    </Select.Root>
+    </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
-import Dropzone from "svelte-file-dropzone";
+	import Fileitem from "$lib/components/Fileitem.svelte";
+  import { FileUpload } from "melt/builders";
+	import { SvelteSet } from "svelte/reactivity";
+  import UploadIcon from "~icons/tabler/cloud-upload";
+  import XIcon from "~icons/tabler/x";
+
 
     const extensions = {
       image: [
@@ -54,18 +59,19 @@ import Dropzone from "svelte-file-dropzone";
     "video/*": [],
   };
 
-  const files = {
-        accepted: [] as File[],
-        rejected: [] as File[],
-    };
+  const fileUpload = new FileUpload({
+  accept: 'image/*,video/*,audio/*',
+  multiple: true
+  });
 
-    function handleFilesSelect(e) {
-        let { acceptedFiles, fileRejections } = e.detail;
-        files.accepted = [...files.accepted, ...acceptedFiles];
-        files.rejected = [...files.rejected, ...fileRejections];
-    }
+  let files = $derived.by(() => {
+		if (fileUpload.selected instanceof SvelteSet) {
+			return Array.from(fileUpload.selected);
+		}
+		return [fileUpload.selected].filter((f): f is File => !!f);
+	});
 
-    $inspect(files.accepted);
+  $inspect(files);
 </script>
 <div class="py-28 lg:py-44 flex flex-col lg:flex-row">
   <div class="w-full lg:w-2/6 mb-12 flex items-center justify-center">
@@ -77,15 +83,34 @@ import Dropzone from "svelte-file-dropzone";
         <h3 class="text-2xl lg:text-4xl">فایل های صوتی تصویری خود را به آسانی به فرمت های مختلف تبدیل کنید</h3>
     </div>
     <div class="h-96 mt-20 flex justify-center">
+      {#if files.length === 0}
       <div id="dropzone" class="w-10/12">
-        <Dropzone 
-          name="File Selector"
-          containerClasses="h-full flex items-center justify-center text-center" 
-          accept="image/*,audio/*,video/*"
-          multiple = {true}
-          on:drop={handleFilesSelect}
-          />
+        <input {...fileUpload.input}/>
+        <div class="relative flex w-full h-full cursor-pointer
+				  flex-col items-center justify-center gap-4
+				  rounded-xl border-2 border-dashed border-gray-300 bg-slate-100
+				  p-6 text-center transition-colors
+				  hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700" 
+          class:!border-accent-500={fileUpload.isDragging} 
+          {...fileUpload.dropzone}>
+          {#if fileUpload.isDragging}
+          <p class="text-accent-400 font-medium">Drop files here</p>
+          {:else}
+            <UploadIcon class="text-4xl" />
+					  <p class="text-md text-gray-500 dark:text-gray-400">
+						  <span class="font-semibold text-gray-900 dark:text-white">Click to upload</span>
+						  or drag and drop
+					  </p>
+          {/if}
+        </div>
     </div>
+    {:else}
+    <div class=" w-full h-full flex flex-col gap-8 items-center">
+      {#each files as file }
+      <Fileitem title = {file.name} filetype = {file.type}/>
+      {/each}
+    </div>
+    {/if}
     </div>
   </div>
   <div class="w-full lg:w-2/6 flex items-center justify-center">
